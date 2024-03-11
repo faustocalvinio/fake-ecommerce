91762d46cda3fa8b6a2f58a134d74fe088242ca6
Fausto Calviño <faustocalvino@outlook.com>
2024-03-11 11:22:18 +0100

    adding stale time and changing readme

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 App with zustand, react query.
 
+
+## Used
+
+- react-hot-toast
+- react-query
+- react-router-dom
+- zustand
+- tailwindcss
+
 ### ToDo
 
 1. React router implementation
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@ import App from "./App.tsx";
 import "./main.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+   defaultOptions: {
+      queries: {
+         staleTime:  60 * 10000,
+      },
+   },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <QueryClientProvider client={queryClient}>
